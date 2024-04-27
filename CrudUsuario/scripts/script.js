9506930e4eb51176734681aa0bb27183ab020658
Amanda De queiroz silva <162377391+silvaaq@users.noreply.github.com>
2024-04-26 21:26:09 -0300

    Add files via upload

--- CrudUsuario/scripts/script.js
+++ CrudUsuario/scripts/script.js
@@ -1,200 +1,180 @@
 const openModal = () => {
-  //document.querySelector('h2').innerText = "Novo usuario"
-//document.getElementById('saveValues').innerText = ('salvar');
-
-document.getElementById('saveValues').removeEventListener('click', addUser);
-
-  document.getElementById('modal').classList.add('active');
-}
-
-const closeModal = () => {
-  document.getElementById('modal').classList.remove('active');
-}
-
-const CapturarValores = () => {
-
-  let listaUsuario = [];
-
-  const nome = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const cel = document.getElementById('cel').value;
-  const cidade = document.getElementById('city').value;
-
-  const dadosUsuario = {
-      id: Date.now(),
-      nome: nome,
-      email: email,
-      cel: cel,
-      cidade: cidade   
-  }
-
-  if(localStorage.getItem('usuarioCadastrados')) {
-      listaUsuario = JSON.parse(localStorage.getItem('usuarioCadastrados'));
+    document.getElementById('modal').classList.add('active');
   }
   
-  listaUsuario.push(dadosUsuario);
-
-  localStorage.setItem('usuarioCadastrados', JSON.stringify(listaUsuario));
-
-  closeModal();
-
-   //document.getElementById('salvar').addEventListener('click', closeModal);
-
-  window.location.reload();
-}
-
-document.getElementById('salvar').addEventListener('click', CapturarValores);
-
-document.getElementById('cancelar').addEventListener('click', closeModal);
-
-function obterUsuarios() {
-  return JSON.parse(localStorage.getItem('usuarioCadastrados'));
-}
-
-function CarregarUsuarios() {
-  
-  let listaUsuario = obterUsuarios();
-  let tabela = document.getElementById('corpo-tabela');
-
-  if(listaUsuario.length === 0) {    
-      tabela.innerHTML = `
-          <tr>
-              <td colspan='6'> Nenhum usuário cadastrado </td>
-          </tr>
-          `
-  }
-  else {
-      MontarTabela(listaUsuario);
-  }
-} 
-  
-window.addEventListener('DOMContentLoaded', () => CarregarUsuarios());
-
-function MontarTabela(listaDeCadastrados) {
-  let tabela = document.getElementById('corpo-tabela');
-
-  let template = '';
-
-  listaDeCadastrados.forEach(pessoa => {
-      template += `
-          <tr> 
-              <td data-cell='nome'> ${pessoa.nome}</td>
-              <td data-cell='email'> ${pessoa.email}</td>
-              <td data-cell='cel'> ${pessoa.cel}</td>
-              <td data-cell='cidade'> ${pessoa.cidade}</td>
-              <td>
-                  <button type="button" class="button green" id="editar">Editar</button>
-                  <button type="button" class="button red excluir" data-id="${pessoa.id}">Excluir</button>
-              </td>
-          </tr>
-      `
-  });
-
-  tabela.innerHTML = template;
-}
-
-document.getElementById('corpo-tabela').addEventListener('click', function (event) {
-
-  const target = event.target;
-  if(target.classList.contains( 'excluir')) {
-      const userId = target.getAttribute('data-id');
-      deletarUsuario(userId);
-  }
-  else if(target.classList.contains('editar')) {
-      const userId = target.getAttribute('data-id');
-      editarUsuario(userId);
-  }
-})
-
-function deletarUsuario(userId) {
-
-  let listaUsuario = JSON.parse(localStorage.getItem('usuarioCadastrados')) || [];
-  const indiceDelete = listaUsuario.findIndex(usuario => usuario.id.toString() === userId);
-
-  if(indiceDelete === -1) {
-      alert('Usuário não encontrado.');
-      return;
+  const closeModal = () => {
+    document.getElementById('modal').classList.remove('active');
   }
   
-  const nomeUsuario = listaUsuario[indiceDelete].nome;
-
-  const confirmacao = window.confirm(`Você realmente deseja excluir o usuário ${nomeUsuario}?`);
+  const CapturarValores = () => {
   
-  if(confirmacao) {
-      listaUsuario.splice(indiceDelete, 1);
-      localStorage.setItem('usuarioCadastrados', JSON.stringify(listaUsuario));
-
-      CarregarUsuarios();
+    let listaUsuario = [];
+  
+    const nome = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const cel = document.getElementById('cel').value;
+    const cidade = document.getElementById('city').value;
+  
+    const dadosUsuario = {
+        id: Date.now(),
+        nome: nome,
+        email: email,
+        cel: cel,
+        cidade: cidade   
+    }
+  
+    if(localStorage.getItem('usuarioCadastrados')) {
+        listaUsuario = JSON.parse(localStorage.getItem('usuarioCadastrados'));
+    }
+    
+    listaUsuario.push(dadosUsuario);
+  
+    localStorage.setItem('usuarioCadastrados', JSON.stringify(listaUsuario));
+  
+     document.getElementById('salvar').addEventListener('click', closeModal);
+  
+    window.location.reload();
+  }
+  
+  document.getElementById('salvar').addEventListener('click', CapturarValores);
+  
+  document.getElementById('cancelar').addEventListener('click', closeModal);
+  
+  function obterUsuarios() {
+    return JSON.parse(localStorage.getItem('usuarioCadastrados'));
+  }
+  
+  function CarregarUsuarios() {
+    
+    let listaUsuario = obterUsuarios();
+    let tabela = document.getElementById('corpo-tabela');
+  
+    if(listaUsuario.length === 0) {    
+        tabela.innerHTML = `
+            <tr>
+                <td colspan='6'> Nenhum usuário cadastrado </td>
+            </tr>
+            `
+    }
+    else {
+        MontarTabela(listaUsuario);
+    }
   } 
-}
-
-function editarUsuario(userId) {
-
-  let listaUsuario = JSON.parse(localStorage.getItem('usuarioCadastrados')) || [];
-  const indiceUpdate = listaUsuario.findIndex(usuario => usuario.id.toString() === userId);
- 
+    
+  window.addEventListener('DOMContentLoaded', () => CarregarUsuarios());
   
-}
-
-document.getElementById('cadastrarUsuario').addEventListener('click', openModal);
-document.getElementById('modalClose').addEventListener('click', closeModal);
-
-//function addUser () {
-
-//let listUser = []
-
-const id= Math.floor(Math.random()* 100)
-//const nome = document.getElementById('nome').value;
-//const email = document.getElementById('E-mail').value;
-//const cel = document.getElementById('celular').value;
-//const cidade = document.getElementById('cidade').value;
-
-
-//const objUser = {
-  idUser : id,
- // nome: nome,
- // email: email,
- //cel: cel,
- // cidade: cidade,
-//}
-
-//if ( localStorage.getItem('cadastroUsuarios')){
-//listUser = JSON.parse(localStorage.getItem('cadastroUsuarios'));
-//}
-
-//listUser.push(objUser);
-
-//closeModal();
-
-//localStorage.setItem('cadastroUsuarios', JSON.stringify(listUser));
-
-
-
-//}
-
-function UpdateUser(){
-  openModal();
-
-  const textTitleUpdate = document.querySelector('h2');
-  textTitleUpdateUser.innerText = "Atualizar Usuario";
-
-  document.getElementById('saveValues').innerText= 'Atualizar';
+  function MontarTabela(listaDeCadastrados) {
+    let tabela = document.getElementById('corpo-tabela');
   
-  const getUserData = JSON.parse(localStorage.getItem("CadastroUsuarios"));
-
-  const userData = getUserData.find(identificarUsuario => identificarUsuario.userId === id);
- document.getElementById("name").value = userData.nomeUser;
- document.getElementById('email').value = userData.emailUser;
- document.getElementById('cel').value = userData.celUser;
- document.getElementById('city').value = userData.cityUser;
-
- document.getElementById('saveValues').addEventListener('click', UpdateUserInfo);
+    let template = '';
   
-}
+    listaDeCadastrados.forEach(pessoa => {
+        template += `
+            <tr> 
+                <td data-cell='nome'> ${pessoa.nome}</td>
+                <td data-cell='email'> ${pessoa.email}</td>
+                <td data-cell='cel'> ${pessoa.cel}</td>
+                <td data-cell='cidade'> ${pessoa.cidade}</td>
+                <td>
+                    <button type="button" class="button green editar" data-id="${pessoa.id}">Editar</button>
+                    <button type="button" class="button red excluir" data-id="${pessoa.id}">Excluir </button>
 
-//function updateUserInfo(){
- // const newName = document.getElementById('nome').value;
- // const newEmail = document.getElementById(email).value;
- // const newCel = document.getElementById('cel').value;
-//  const newCity = document.getElementById('city').value;
+                </td>
+            </tr>
+        `
+    });
+  
+    tabela.innerHTML = template;
+  }
+  
+  document.getElementById('corpo-tabela').addEventListener('click', function (event) {
+  
+    const target = event.target;
+    if(target.classList.contains( 'excluir')) {
+        const userId = target.getAttribute('data-id');
+        deletarUsuario(userId);
+    }
+    else if(target.classList.contains('editar')) {
+        const userId = target.getAttribute('data-id');
+        editarUsuario(userId);
+    }
+  })
+  
+  function deletarUsuario(userId) {
+  
+    let listaUsuario = JSON.parse(localStorage.getItem('usuarioCadastrados')) || [];
+    const indiceDelete = listaUsuario.findIndex(usuario => usuario.id.toString() === userId);
+  
+    if(indiceDelete === -1) {
+        alert('Usuário não encontrado.');
+        return;
+    }
+    
+    const nomeUsuario = listaUsuario[indiceDelete].nome;
+  
+    const confirmacao = window.confirm(`Você realmente deseja excluir o usuário ${nomeUsuario}?`);
+    
+    if(confirmacao) {
+        listaUsuario.splice(indiceDelete, 1);
+        localStorage.setItem('usuarioCadastrados', JSON.stringify(listaUsuario));
+  
+        CarregarUsuarios();
+    } 
+  }
+  
+  function editarUsuario(userId) {
+  
+    let listaUsuario = JSON.parse(localStorage.getItem('usuarioCadastrados')) || [];
+    const indiceUpdate = listaUsuario.findIndex(usuario => usuario.id.toString() === userId);
+  
+  
+  
+  }
+  
+  document.getElementById('cadastrarUsuario').addEventListener('click', openModal);
+  document.getElementById('modalClose').addEventListener('click', closeModal);
+  
+  function addUser () {
+  
+  let listUser = []
+  
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('E-mail').value;
+  const cel = document.getElementById('celular').value;
+  const cidade = document.getElementById('cidade').value;
+  
+  const objUser = {
+    nome: nome,
+    email: email,
+    cel: cel,
+    cidade: cidade,
+  }
+  
+  if ( localStorage.getItem('cadastroUsuarios')){
+  listUser = JSON.parse(localStorage.getItem('cadastroUsuarios'));
+  }
+  
+  listUser.push(objUser);
+  
+  closeModal();
+  
+  localStorage.setItem('cadastroUsuarios', JSON.stringify(listUser));
+  
+  
+  
+  }
 
+  function deleteUser(id){
+    const getUserData = JSON.parse(localStorage.getItem("listaUsuasrios"));
+
+    const findUser = getUserData.find((user) => user.idUser == id);
+
+   // if (findUser !== -1){
+        getUserData.splice(findUser, 1);
+
+        //localStorage.setItem("listaUsuarios" JSON.stringify(getUserdata));
+        window.location.reload();
+    //}
+
+    
+  }
